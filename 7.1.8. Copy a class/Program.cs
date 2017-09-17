0477b086c903114d9424c4cd8471c623ad6a4282
FxFn1131 <FxFn1131@DARKSIDE>
2017-09-17 11:24:43 +0300

    Sınıflar devam ediyor..

--- 7.1.8. Copy a class/Program.cs
+++ 7.1.8. Copy a class/Program.cs
@@ -3,6 +3,34 @@
 class MyClass
 {
     public int x;
+
+    public MyClass()
+    {
+        Console.WriteLine("Normal Constructer...");
+    }
+    static MyClass()
+    {
+        Console.WriteLine("Static Constructer...");
+    }
+
+   
+
+}
+
+class A
+{
+public int x { get; set; }
+   
+
+
+}
+
+class B : A
+{
+
+    public int y { get; set; }
+
+
 }
 
 class MainClass
@@ -14,6 +42,7 @@ class MainClass
 
         a.x = 10;
         b.x = 20;
+     
 
         Console.WriteLine("a.x {0}, b.x {1}", a.x, b.x);
 
@@ -21,8 +50,20 @@ class MainClass
         b.x = 30;
 
         Console.WriteLine("a.x {0}, b.x {1}", a.x, b.x);
+
+
+        A ab = new B();
+        B ba= (B)ab; // downcast
+        B b1 = new B();
+        A a1 = b1;//upcast
+        
+       
+        
     }
 }
+
+
+
 
 //a.x 10, b.x 20
 //a.x 30, b.x 30
