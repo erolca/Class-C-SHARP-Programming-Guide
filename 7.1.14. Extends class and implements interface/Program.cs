a845c45701f47640e516d4d48102bd043edaeefe
erolcalisgan <erolcalisgan@hotmail.com>
2017-09-20 14:06:26 +0300

    7.1.Class Definition

---
+++ 7.1.14. Extends class and implements interface/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+interface Talkable
+{
+    string Table();
+}
+
+class Animal { }
+
+class Cat : Animal, Talkable
+{
+    string Talkable.Table()
+    {
+        return "miao";
+    }
+}
+
+class Dog : Animal, Talkable
+{
+    string Talkable.Table()
+    {
+        return "bulk";
+    }
+}
+
+class Elephant : Animal
+{
+
+}
+
+class MainClass
+{
+    static void Main()
+    {
+        Animal[] AnimalArray = new Animal[3];
+
+        AnimalArray[0] = new Cat();
+        AnimalArray[1] = new Elephant();
+        AnimalArray[2] = new Dog();
+
+        foreach (Animal a in AnimalArray)
+        {
+            Talkable b = a as Talkable;
+            if (b != null)
+                Console.WriteLine("Baby is called: {0}", b.Table());
+        }
+    }
+}
