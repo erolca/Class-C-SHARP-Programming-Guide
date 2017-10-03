8c9f93dfc1bd37608462a815417f300b61b6d4ba
FxFn1131 <FxFn1131@DARKSIDE>
2017-10-03 22:34:03 +0300

    7.9.Method Recursion

---
+++ Directory.GetDirectories/Class1.cs
@@ -0,0 +1,21 @@
+﻿
+//File.Exists Yöntemi
+//Belirtilen dosyanın var olup olmadığını belirler.
+
+/* string curFile = @"c:\temp\test.txt";
+* Console.WriteLine(File.Exists(curFile) ? "File exists." : "File does not exist.");
+*/
+
+//Directory.Exists
+//Verilen yol disk üzerinde var olan bir dizin başvurduğu olup olmadığını belirler.
+
+
+//Directory.GetFiles Yöntemi
+//Belirtilen dizinde(yollarına dahil) dosya adlarını döndürür.
+
+//Directory.GetDirectories
+//Belirtilen dizindeki alt dizinleri (yollarına dahil) adlarını döndürür.
+
+
+//Directory.GetCurrentDirectory Yöntemi 
+//Uygulamanın geçerli çalışma dizini alır.
