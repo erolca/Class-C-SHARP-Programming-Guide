88de7cc4e5edb61a0bddc54e8c0e37a3ae5c54b5
erolcalisgan <erolcalisgan@hotmail.com>
2017-10-19 17:18:39 +0300

    7.23.virtual

---
+++ 7.23.13. Virtual Accessor/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+public class Class1
+{
+    public static void Main(string[] strings)
+    {
+        MyMoney dm = new MyMoney();
+        dm.CurrencyValue = 2.50M;
+        Console.WriteLine(dm.CurrencyValue);
+
+        Dollar dollar = new Dollar();
+        dollar.InitDollar(1.2M);
+
+        dollar.EuroValue = dm.EuroValue;
+        Console.WriteLine(dollar.CurrencyValue);
+
+        dollar.CurrencyValue = 2 * dollar.CurrencyValue;
+        dm.EuroValue = dollar.EuroValue;
+        Console.WriteLine(dm.CurrencyValue);
+
+    }
+}
+
+abstract public class Currency
+{
+    private decimal mEuroValue = 0M; // [Euro]
+
+    public abstract decimal CurrencyValue { get; set; }
+    public decimal EuroValue
+    {
+        get
+        {
+            return mEuroValue;
+        }
+        set
+        {
+            mEuroValue = value;
+        }
+    }
+}
+
+public class MyMoney : Currency
+{
+    private static decimal mDMtoEuro = 1.96M;
+    public override decimal CurrencyValue
+    {
+        get { return EuroValue * mDMtoEuro; }
+        set { EuroValue = value / mDMtoEuro; }
+    }
+}
+
+public class Dollar : Currency
+{
+    public decimal mDollartoEuroConversion;
+
+    public void InitDollar(decimal mDollartoEuro)
+    {
+        mDollartoEuroConversion = mDollartoEuro;
+    }
+
+    public override decimal CurrencyValue
+    {
+        get { return EuroValue * mDollartoEuroConversion; }
+        set { EuroValue = value / mDollartoEuroConversion; }
+    }
+}
