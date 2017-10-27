68933c6299ee82ad26a5a578497c99a446bb5413
FxFn1131 <FxFn1131@DARKSIDE>
2017-10-27 22:53:48 +0300

    7.50.MemberwiseClone

---
+++ 7.50.2. Use MemberwiseClone/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+//7.50.2.	Use MemberwiseClone method to clone object
+class MyValue
+{
+    public MyValue(int count)
+    {
+        this.count = count;
+    }
+    public int count;
+}
+class MyObject
+{
+    public MyValue contained;
+
+    public MyObject(int count)
+    {
+        this.contained = new MyValue(count);
+    }
+    public MyObject Clone()
+    {
+        Console.WriteLine("Clone");
+        return ((MyObject)MemberwiseClone());
+    }
+    
+}
+class MainClass
+{
+    public static void Main()
+    {
+        MyObject my = new MyObject(3);
+        MyObject myClone = my.Clone();
+        Console.WriteLine("Values: {0} {1}", my.contained.count, myClone.contained.count);
+        myClone.contained.count = 1;
+        Console.WriteLine("Values: {0} {1}", my.contained.count, myClone.contained.count);
+    }
+}
+//Clone
+//Values: 3 3
+//Values: 1 1
---
+++ 7.50.3. Perform a memberwise clone/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+//7.50.3.	Perform a memberwise clone of myEmployee using the Employee.Copy() method
+
+public class Employee
+{
+    public string firstName;
+    public string lastName;
+
+    public Employee(string firstName, string lastName)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("firstName = " + firstName);
+        Console.WriteLine("lastName = " + lastName);
+    }
+
+    public static Employee Copy(Employee Employee)
+    {
+        return (Employee)Employee.MemberwiseClone();
+    }
+}
+
+
+class MainClass
+{
+
+    public static void Main()
+    {
+        Console.WriteLine("Creating Employee objects");
+        Employee myEmployee = new Employee("A", "M");
+        Employee myOtherEmployee = new Employee("B", "N");
+        Console.WriteLine("myEmployee details:");
+        myEmployee.Display();
+        Console.WriteLine("myOtherEmployee details:");
+        myOtherEmployee.Display();
+
+
+        Console.WriteLine("Performing a memberwise clone of myEmployee to myOldEmployee");
+        Employee myOldEmployee = Employee.Copy(myEmployee);
+        Console.WriteLine("myOldEmployee details:");
+        myOldEmployee.Display();
+    }
+}
+//Creating Employee objects
+//myEmployee details:
+//firstName = A
+//lastName = M
+//myOtherEmployee details:
+//firstName = B
+//lastName = N
+//Performing a memberwise clone of myEmployee to myOldEmployee
+//myOldEmployee details:
+//firstName = A
+//lastName = M
+
+
+
+
+
+
+
+
