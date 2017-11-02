19f34c619374821d1ec7e6771e6223bbf6cda4ba
erolcalisgan <erolcalisgan@hotmail.com>
2017-11-02 14:30:37 +0300

    7.61.Partial Class

---
+++ 7.61.1. generic partial class/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+
+partial class Example<TFirst, TSecond> : IEquatable<string> where TFirst : class
+{
+    public bool Equals(string other)
+    {
+        return false;
+    }
+}
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
